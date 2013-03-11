879d0c23959648bf5c103a8da853f8ce4e91adfe
Fred Sharples <fred@orangedesign.com>
2013-03-11 12:55:26 -0700

    Added STESimpleTextDocument class

---
+++ SimpleTextEditor/STESimpleTextDocument.h
@@ -0,0 +1,13 @@
+//
+//  STESimpleTextDocument.h
+//  SimpleTextEditor
+//
+//  Created by Fred Sharples on 3/11/13.
+//  Copyright (c) 2013 Orange Design. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface STESimpleTextDocument : UIDocument
+
+@end
---
+++ SimpleTextEditor/STESimpleTextDocument.m
@@ -0,0 +1,13 @@
+//
+//  STESimpleTextDocument.m
+//  SimpleTextEditor
+//
+//  Created by Fred Sharples on 3/11/13.
+//  Copyright (c) 2013 Orange Design. All rights reserved.
+//
+
+#import "STESimpleTextDocument.h"
+
+@implementation STESimpleTextDocument
+
+@end
