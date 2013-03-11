e10e5ca9f33191b6ad7a5ef49d4dcabf98497dce
Fred Sharples <fred@orangedesign.com>
2013-03-11 13:37:03 -0700

    Added document handing code

--- SimpleTextEditor/STEMasterViewController.m
+++ SimpleTextEditor/STEMasterViewController.m
@@ -10,6 +10,14 @@
 
 #import "STEDetailViewController.h"
 
+#import "STESimpleTextDocument.h"
+
+NSString* STEDocFilenameExtension = @"stedoc";
+
+NSString* DisplayDetailSegue = @"DisplayDetailSegue";
+
+NSString* STEDocumentsDirectoryName = @"Documents";
+
 @interface STEMasterViewController () {
     NSMutableArray *_objects;
 }
@@ -17,8 +25,86 @@
 
 @implementation STEMasterViewController
 
+NSMutableArray * documents;
+
+- (IBAction)addDocument:(id)sender {
+    // Disable the Add button while creating the document.
+    self.addButton.enabled = NO;
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        // Create the new URL object on a background queue.
+        NSFileManager *fm = [NSFileManager defaultManager];
+        NSURL *newDocumentURL = [fm URLForUbiquityContainerIdentifier:nil];
+        
+        newDocumentURL = [newDocumentURL
+                          URLByAppendingPathComponent:STEDocumentsDirectoryName
+                          isDirectory:YES];
+        newDocumentURL = [newDocumentURL
+                          URLByAppendingPathComponent:[self newUntitledDocumentName]];
+        
+        // Perform the remaining tasks on the main queue.
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // Update the data structures and table.
+            [documents addObject:newDocumentURL];
+            
+            // Update the table.
+            NSIndexPath* newCellIndexPath =
+            [NSIndexPath indexPathForRow:([documents count] - 1) inSection:0];
+            [self.tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:newCellIndexPath]
+                                  withRowAnimation:UITableViewRowAnimationAutomatic];
+            
+            [self.tableView selectRowAtIndexPath:newCellIndexPath
+                                        animated:YES
+                                  scrollPosition:UITableViewScrollPositionMiddle];
+            
+            // Segue to the detail view controller to begin editing.
+            UITableViewCell* selectedCell = [self.tableView
+                                             cellForRowAtIndexPath:newCellIndexPath];
+            [self performSegueWithIdentifier:DisplayDetailSegue sender:selectedCell];
+            
+            // Reenable the Add button.
+            self.addButton.enabled = YES;
+        });
+    });
+}
+
+
+- (NSString*)newUntitledDocumentName {
+    NSInteger docCount = 1;     // Start with 1 and go from there.
+    NSString* newDocName = nil;
+    
+    // At this point, the document list should be up-to-date.
+    BOOL done = NO;
+    while (!done) {
+        newDocName = [NSString stringWithFormat:@"Note %d.%@",
+                      docCount, STEDocFilenameExtension];
+        
+        // Look for an existing document with the same name. If one is
+        // found, increment the docCount value and try again.
+        BOOL nameExists = NO;
+        for (NSURL* aURL in documents) {
+            if ([[aURL lastPathComponent] isEqualToString:newDocName]) {
+                docCount++;
+                nameExists = YES;
+                break;
+            }
+        }
+        
+        // If the name wasn't found, exit the loop.
+        if (!nameExists)
+            done = YES;
+    }
+    return newDocName;
+}
+
+
+
 - (void)awakeFromNib
 {
+    if (!documents){
+        documents = [[NSMutableArray alloc] init];
+    }
+    
     [super awakeFromNib];
 }
 
